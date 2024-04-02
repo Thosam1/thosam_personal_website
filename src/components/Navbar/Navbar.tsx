960f7c292069ae778912f38dc775ea704facf8b4
Thosam1 <thosamtenzin2002@gmail.com>
2024-04-03 00:39:54 +0200

    cursor-pointer on logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -18,54 +18,16 @@ const Navbar: React.FC = () => {
   };
 
 
-  // animation downward window
-  // const menuVars = {
-  //   initial: {
-  //     scaleY: 0,
-  //   },
-  //   animate: {
-  //     scaleY: 1,
-  //     transition: {
-  //       duration: 0.5,
-  //       ease: [0.12, 0, 0.39, 0],
-  //     },
-  //   },
-  //   exit: {
-  //     scaleY: 0,
-  //     transition: {
-  //       delay: 0.5,
-  //       duration: 0.5,
-  //       ease: [0.22, 1, 0.36, 1],
-  //     },
-  //   },
-  // };
-
-  const containerVars = {
-    initial: {
-      transition: {
-        staggerChildren: 0.09,
-        staggerDirection: -1,
-      },
-    },
-    open: {
-      transition: {
-        delayChildren: 0.3,
-        staggerChildren: 0.09,
-        staggerDirection: 1,
-      },
-    },
-  };
-
   return (
     <header className="flex items-center justify-center">
       <nav className="flex justify-between items-center py-8 px-8 w-full max-w-screen-md">
-        <div className="flex items-center">
+        <Link className="flex items-center cursor-pointer" href={'/'}>
           <span className="font-semibold">
             {FULL_NAME}
           </span>
-        </div>
+        </Link>
 
-        <div className="lg:flex hidden gap-12 text-md text-zinc-400">
+        <div className="lg:flex hidden gap-12 text-md text-zinc-100">
 
           {NAV_LINKS.map((link) => (
             <Link href={link.href} key={link.key} className="font-medium text-gray-500 hover:text-gray-400 py-3 md:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
@@ -106,7 +68,9 @@ const Navbar: React.FC = () => {
             <div className="flex h-full flex-col">
 
               <div className="flex justify-between items-center w-full max-w-screen-md">
-                <span className="font-semibold">{FULL_NAME}</span>
+                <Link className="flex items-center cursor-pointer" href={'/'} onClick={toggleNav}>
+                  <span className="font-semibold">{FULL_NAME}</span>
+                </Link>
                 {/* <p
                     className="cursor-pointer text-md text-black"
                     onClick={toggleNav}
@@ -132,27 +96,6 @@ const Navbar: React.FC = () => {
                 })}
 
               </motion.div>
-
-              {/* <motion.div
-                variants={containerVars}
-                initial="initial"
-                animate="open"
-                exit="initial"
-                className="flex flex-col h-full justify-center font-lora items-center gap-4 "
-              >
-                {NAV_LINKS.map((link, index) => {
-                  return (
-                    <div className="overflow-hidden">
-                      <MobileNavLink
-                        key={index}
-                        title={link.label}
-                        href={link.href}
-                      />
-                    </div>
-                  );
-                })}
-              </motion.div> */}
-
             </div>
           </motion.div>
         )}
@@ -162,30 +105,3 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
-const mobileLinkVars = {
-  initial: {
-    y: "30vh",
-    transition: {
-      duration: 0.5,
-      ease: [0.37, 0, 0.63, 1],
-    },
-  },
-  open: {
-    y: 0,
-    transition: {
-      ease: [0, 0.55, 0.45, 1],
-      duration: 0.7,
-    },
-  },
-};
-const MobileNavLink = ({ title, href }: any) => {
-  return (
-    <motion.div
-      variants={mobileLinkVars}
-      className="text-5xl uppercase text-black"
-    >
-      <Link href={href}>{title}</Link>
-    </motion.div>
-  );
-};
-
