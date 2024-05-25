ff1aafad717c3d575d8c7d1eaafcd4a689ab2865
norl-adn <thoesam.norlha-tsang@adnovum.ch>
2024-05-25 17:03:38 +0200

    trying out wo json

--- src/app/db/utils.ts
+++ src/app/db/utils.ts
@@ -2,12 +2,12 @@ import { next } from 'sucrase/dist/types/parser/tokenizer';
 
 export async function getBlogPostViews(slug: string) {
 	let viewCount = 0;
-	await fetch(`/api/views/${slug}`, {
+	await fetch(`${process.env.WEBSITE_URL}/api/views/${slug}`, {
 		method: 'GET',
 		next: { revalidate: 1 }
 	})
-		.then(r => r.json())
-		.then(data => viewCount = data.total);
+		//.then(r => r.json())
+		//.then(data => viewCount = data.total);
 	console.log("after getting blog post view count : ", slug, viewCount)
 	return viewCount;
 }
